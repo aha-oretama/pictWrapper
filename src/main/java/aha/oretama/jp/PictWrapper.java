d35647ca1346bd4bc4b9d53a4f3079e3a2a546ff
aha-oremata <sekine_y_529@msn.com>
2016-12-18 22:22:25 +0900

    Initial Commit

---
+++ src/main/java/aha/oretama/jp/PictWrapper.java
@@ -0,0 +1,8 @@
+package aha.oretama.jp;
+
+/**
+ * @author aha-oretama
+ * @Date 2016/12/18
+ */
+public class PictWrapper {
+}
---
+++ src/test/groovy/aha/oretama/jp/PictWrapperTest.groovy
@@ -0,0 +1,10 @@
+package aha.oretama.jp
+
+import spock.lang.Specification
+
+/**
+ * @author aha-oretama
+ * @Date 2016/12/18
+ */
+class PictWrapperTest extends Specification {
+}
